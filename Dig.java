13788209dd50c31dce62950053bb012a74d79c7f
sourcecodeQC <alek.de.mehlem@gmail.com>
2023-11-30 14:25:33 +0100

    Documentation added

--- Dig.java
+++ Dig.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; // import scanner... duh
 
 public class Dig {
     public static void main(String[] args) {
@@ -7,12 +7,17 @@ public class Dig {
         double num = reader.nextDouble(); // store input
         reader.close(); // close readder - saving resources
 
-        if (99 < num && num < 1000 && num % 111 == 0 || num % 11 ==0 || num % 110 == 0){
-            System.out.println("3 diggit number. Not unique");}
-        else if(99 < num && num < 1000){
-            System.out.println("Thee unique");}
-        else{
-            System.out.println("Not 3 diggit number");
+        if (99 < num && num < 1000 && num % 111 == 0 || num % 11 ==0 || num % 110 == 0) // check if num is 3 diggit number but not unique
+        { 
+            System.out.println("3 diggit number. Not unique");
+        }
+        else if(99 < num && num < 1000) // check if num is 3 diggit number and unique
+        { 
+            System.out.println("Thee unique");
+        }
+        else
+        {
+            System.out.println("Not 3 diggit number"); // if num is not 3 diggit number
         }
         
 
---
+++ Sevenn.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Sevenn {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: "); // collect input
+    }
+    
+}
