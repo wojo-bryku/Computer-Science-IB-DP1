2922f1a9d66cde54c3300cd0b47450c16eae4e44
sourcecodeQC <alek.de.mehlem@gmail.com>
2023-12-21 23:40:14 +0100

    Create Digisumint.java

---
+++ Loops/Digisumint.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Digisumint {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);  // Setting up the scanner
+
+        System.out.println("Enter number"); //prompt user
+        int num = reader.nextInt(); //read input
+        reader.close(); //closing scanner - saving memory
+
+
+        int amd = 0; // amd - ammout of all digits
+        while(0 != num)
+        {
+            num %= 10;
+            amd++;
+        }
+
+        System.out.println("# of dig is: " + amd);
+
+    }
+}
