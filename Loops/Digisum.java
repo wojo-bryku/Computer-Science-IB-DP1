74175e2587ab032f493de33afe67027d26399755
sourcecodeQC <alek.de.mehlem@gmail.com>
2023-12-21 23:31:50 +0100

    Update Digisum.java

--- Loops/Digisum.java
+++ Loops/Digisum.java
@@ -1,10 +1,28 @@
 import java.util.Scanner;
 
 public class Digisum {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         
-
+        Scanner reader = new Scanner(System.in);  // Setting up the scanner
         
+        System.out.println("Enter number"); //prompt user
+        int num = reader.nextInt(); //read input
+        reader.close(); //closing scanner - saving memory
+
+
+        int sum = 0;
+        while(num != 0)
+        {
+            int lastDig = num % 10;
+            sum += lastDig;
+            num /= 10;
+        }
+
+
+        System.out.println("Sum is: " + sum);
     }
 
 }
