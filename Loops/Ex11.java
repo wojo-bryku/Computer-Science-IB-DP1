05cb2cff56d9b5acd4f0e953d3b309ee55cb24a7
sourcecodeQC <69313475+sourcecodeQC@users.noreply.github.com>
2024-01-04 15:46:51 +0100

    work in progress

--- Loops/Ex11.java
+++ Loops/Ex11.java
@@ -1,3 +1,5 @@
+//Exercise 11: Write a program to print Fibonacci series up to n terms and their sum.
+
 import java.util.Scanner;
 
 public class Ex11 {
@@ -6,6 +8,16 @@ public class Ex11 {
 
         System.out.println("Enter the number of terms for the Fibonacci series: ");
         int n = reader.nextInt();
-        
+
+        int[] fib = new int[n];
+
+        fib[0] = 0;
+        fib[1] = 1;
+
+        System.out.println("Fibonacci series to: " + n + " terms");
+        System.out.println(fib[0] + " + " + fib[1]);
+
+
+
     }
 }
