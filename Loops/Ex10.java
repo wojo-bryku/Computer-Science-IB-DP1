96c69fef8701ef971f2ff9f9ecdcc60004408e7f
sourcecodeQC <69313475+sourcecodeQC@users.noreply.github.com>
2024-01-04 14:59:30 +0100

    Change of file name Ex9=>Ex10

---
+++ Loops/Ex10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Ex10 {
+    public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    Random random = new Random();
+
+    //System.out.println("Enter number");
+    int num = random.nextInt(101);
+    System.out.println(num);
+    }
+}
