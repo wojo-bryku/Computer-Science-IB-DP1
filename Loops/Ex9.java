5ff56c7aadde68c387b593d7efa10869e675b024
sourcecodeQC <69313475+sourcecodeQC@users.noreply.github.com>
2024-01-04 14:53:08 +0100

    Create Ex9.java

---
+++ Loops/Ex9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Ex9 {
+    public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    System.out.println("Enter number");
+    int num = reader.nextInt();
+    
+    }
+}
