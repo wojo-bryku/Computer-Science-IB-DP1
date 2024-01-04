af86bde171af8c69cdbfad58ea34c1a1a0d74d8e
sourcecodeQC <69313475+sourcecodeQC@users.noreply.github.com>
2024-01-04 15:33:07 +0100

    Update Ex11.java

---
+++ Loops/Ex11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter the number of terms for the Fibonacci series: ");
+        int n = reader.nextInt();
+        
+    }
+}
