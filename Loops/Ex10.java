e7457f2be2953f7233a75be224f4a8a3538bdd64
sourcecodeQC <69313475+sourcecodeQC@users.noreply.github.com>
2024-01-04 15:03:46 +0100

    nie działa >-<

--- Loops/Ex10.java
+++ Loops/Ex10.java
@@ -6,8 +6,27 @@ public class Ex10 {
     Scanner reader = new Scanner(System.in);
     Random random = new Random();
 
-    //System.out.println("Enter number");
+    int userNum = reader.nextInt();
     int num = random.nextInt(101);
-    System.out.println(num);
+
+    while(userNum != num)
+    {
+        if(userNum>num)
+        {
+            System.out.println("Too high");
+            userNum = reader.nextInt();
+        }
+        if (userNum<num)
+        {
+            System.out.println("Too low");
+            userNum = reader.nextInt();
+        }
+        if (userNum == num)
+        {
+            System.out.println("You guessed it!");
+            break;
+        }
+    
+
     }
 }
