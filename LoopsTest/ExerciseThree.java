d6e1842e9a8c7dc2d144a38663f99f05963b0a7f
sourcecodeQC <69313475+sourcecodeQC@users.noreply.github.com>
2024-01-11 15:20:20 +0100

    Ex3 works; Ex2 no

---
+++ LoopsTest/ExerciseThree.java
@@ -0,0 +1,33 @@
+//Write a Java program that calculates the factorial of a user specified number. Apart from the result, the expression for calculating the product should also be printed by the program. Use a while loop to complete this task.
+
+import java.util.Scanner;
+
+
+public class ExerciseThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        int factorial = 1;
+        int i = 1;
+        while (i <= num) {
+            factorial *= i;
+            i++;
+        }
+        System.out.println("The factorial of " + num + " is " + factorial);
+
+        //the expression for calculating the product
+        System.out.println("The expression for calculating the product is: ");
+        int j = 1;
+        while (j <= num) {
+            if (j == num) {
+                System.out.print(j);
+            } else {
+                System.out.print(j + " * ");
+            }
+            j++;
+        }
+
+    }
+}
---
+++ LoopsTest/ExerciseTwo.java
@@ -0,0 +1,46 @@
+//Using basic java Write a Java program that reads 10 numbers from the user. Use a for loop to complete this task. The line responsible for reading in a number should appear only once in your code. The program should then find the sum and average of the entered numbers. The program should also display the largest and smallest numbers entered.
+
+import java.util.Scanner;
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter third number: ");
+        int num3 = sc.nextInt();
+
+        System.out.println("Enter fourth number: ");
+        int num4 = sc.nextInt();
+
+        System.out.println("Enter fifth number: ");
+        int num5 = sc.nextInt();
+
+        System.out.println("Enter sixth number: ");
+        int num6 = sc.nextInt();
+
+        System.out.println("Enter seventh number: ");
+        int num7 = sc.nextInt();
+
+        System.out.println("Enter eighth number: ");
+        int num8 = sc.nextInt();
+
+        System.out.println("Enter ninth number: ");
+        int num9 = sc.nextInt();
+
+        System.out.println("Enter tenth number: ");
+        int num10 = sc.nextInt();
+
+
+        int sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+        int average = sum / 10;
+        
+        System.out.println("The sum of the numbers is: " + sum + "and the average is: "+ average);
+        
+    }
+    
+}
