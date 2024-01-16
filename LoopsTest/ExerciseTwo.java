85c3070a657681c0a685e6e59c0674ca3a7179be
sourcecodeQC <69313475+sourcecodeQC@users.noreply.github.com>
2024-01-16 13:00:37 +0100

    partial fix

--- LoopsTest/ExerciseTwo.java
+++ LoopsTest/ExerciseTwo.java
@@ -5,85 +5,37 @@ import java.util.Scanner;
 public class ExerciseTwo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        int num1 = sc.nextInt();
+        ///The program should also display the largest and smallest numbers entered; use whie/fors loops
+        int i = 0;
+        int sum = 0;
+        int largest = 0;
+        int smallest = 0;
+        int avg = 0;
+        while(i<10)
+        {
+            System.out.println("Enter a " + i + " number: ");
+            int num = sc.nextInt();
+            sum = sum + num;
 
-        System.out.println("Enter second number: ");
-        int num2 = sc.nextInt();
+            if (num>largest)
+            {
+                largest = num;
+            }
+            if (num<smallest)
+            {
+                smallest = num;
+            }
+            i++;
 
-        System.out.println("Enter third number: ");
-        int num3 = sc.nextInt();
-
-        System.out.println("Enter fourth number: ");
-        int num4 = sc.nextInt();
-
-        System.out.println("Enter fifth number: ");
-        int num5 = sc.nextInt();
-
-        System.out.println("Enter sixth number: ");
-        int num6 = sc.nextInt();
-
-        System.out.println("Enter seventh number: ");
-        int num7 = sc.nextInt();
-
-        System.out.println("Enter eighth number: ");
-        int num8 = sc.nextInt();
-
-        System.out.println("Enter ninth number: ");
-        int num9 = sc.nextInt();
-
-        System.out.println("Enter tenth number: ");
-        int num10 = sc.nextInt();
-
-
-        int sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
-        int average = sum / 10;
+        }
         
-        System.out.println("The sum of the numbers is: " + sum + " and the average is: "+ average);
-        
-
-        ///The program should also display the largest and smallest numbers entered; use whie/for
-
-        
-        int largest = num1;
-        int smallest = num1;
-
-        if (num2 > largest) {
-            largest = num2;
-        }
-        if (num3 > largest) {
-            largest = num3;
-        }
-        if (num4 > largest) {
-            largest = num4;
-        }
-        if (num5 > largest) {
-            largest = num5;
-        }
-        if (num6 > largest) {
-            largest = num6;
-        }
-        if (num7 > largest) {
-            largest = num7;
-        }
-        if (num8 > largest) {
-            largest = num8;
-        }
-        if (num9 > largest) {
-            largest = num9;
-        }
-        if (num10 > largest) {
-            largest = num10;
-        }
-
-        
-        
+        avg = sum / 10;
 
 
 
 
 
-
+        System.out.println("The sum of the numbers is: " + sum + " and the average is: "+ avg);
         System.out.println("The largest number is: " + largest + " and the smallest number is: " + smallest);
 
         
