f87b1c3b2affed0253ade570f896da9b62379ea4
sourcecodeQC <aleksander@demehlem.com>
2024-01-25 16:02:23 +0100

    broken xD

---
+++ Loops/Ex14.java
@@ -0,0 +1,28 @@
+//Write a program that prints the pattern below. The number of rows should depend on the user’s input.
+//1
+//2 3
+//4 5 6
+//7 8 9 10
+
+import java.util.Scanner;
+
+public class Ex14 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows: ");
+        int r = reader.nextInt();
+
+        for(int i = 0; i < r; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print(i + j + 1 + " ");
+            }
+            System.out.println();
+            
+        }
+
+    }
+
+}
