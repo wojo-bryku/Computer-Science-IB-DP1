8c2b823ce45497c83a8990015aa5d1f6bd107b39
sourcecodeQC <aleksander@demehlem.com>
2024-02-08 15:32:58 +0100

    Works

--- Strings/Ex1.java
+++ Strings/Ex1.java
@@ -4,9 +4,16 @@ public class Ex1 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Enter a word: ");
         String word = reader.next();
+
+        System.out.println("Enter an index: ");
         int index = reader.nextInt();
 
-        System.out.println(word.substring(0, index));
+        if(index >= 0 && index < word.length())
+            System.out.println("The character at index " + index + " is: " + word.charAt(index));
+        else
+            System.out.println("Invalid index");
+       
     }    
 }
