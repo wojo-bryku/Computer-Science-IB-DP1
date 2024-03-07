1044b5d16f7ce4d41cfaaa6f0e10926819d689fa
sourcecodeQC <aleksander@demehlem.com>
2024-03-07 15:18:03 +0100

    Ex4 and Ex5 doned

--- LoopsRetake/Exercise4.java
+++ LoopsRetake/Exercise4.java
@@ -3,3 +3,23 @@
 
 import java.util.Scanner;
 
+public class Exercise4 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+
+
+    int n = sc.nextInt();
+    String binary = "";
+
+
+    while (n > 0) {
+      binary = (n % 2) + binary;
+      n /= 2;
+    }
+
+
+    System.out.println(binary);
+
+  }
+}
