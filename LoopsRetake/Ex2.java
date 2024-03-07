427a60d097becc6c1151408fc1facb0e53ebbc1c
sourcecodeQC <aleksander@demehlem.com>
2024-03-07 14:53:07 +0100

    LoopTest Ex1 and Ex2

---
+++ LoopsRetake/Ex2.java
@@ -0,0 +1,40 @@
+//Write a Java program that reads 10 numbers from the user. Use a while loop to complete this task. The line responsible for reading in a number should appear only once in your code. The program should then find the sum and average of the entered numbers. The program should also display the largest and smallest numbers entered.
+
+import java.util.Scanner;
+
+
+public class Ex2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int sum = 0;
+    int count = 0;
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
+    while (count < 10) {
+      System.out.println("Enter a number: ");
+      int n = sc.nextInt();
+      sum += n;
+      count++;
+      if (n > max) {
+        max = n;
+      }
+      if (n < min) {
+        min = n;
+      }
+    }
+    System.out.println("Sum: " + sum);
+    System.out.println("Average: " + (sum / 10));
+    System.out.println("Max: " + max);
+    System.out.println("Min: " + min);
+
+   
+
+
+  }
+
+
+
+
+
+
+}
