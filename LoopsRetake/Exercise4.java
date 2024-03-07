fd2ae204b176000cf197a7967dfadc9be1a75b18
sourcecodeQC <aleksander@demehlem.com>
2024-03-07 15:09:11 +0100

    Ex4 created

---
+++ LoopsRetake/Exercise4.java
@@ -0,0 +1,5 @@
+// Write a Java program that converts a user specified decimal number (base 10) into its binary representation (base 2). The binary number should be stored in a String variable. Below you can find an example demonstrating how the conversion can be done for decimal number 13:
+//
+
+import java.util.Scanner;
+
