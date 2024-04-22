9c73f7b4ce8c1e46c8c2d3d4931478688afa6151
scQC <alek.de.mehlem@gmail.com>
2024-04-22 11:25:41 +0200

    program

--- Methods/Isodd.java
+++ Methods/Isodd.java
@@ -1,8 +1,28 @@
 // Introduction to methods JAVA
+import java.util.Scanner;
 
 public class Isodd {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in); // def scanner as sc
+
+
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+       if (isOdd(num)) { //if isOdd is true
+           System.out.println(num + " is an odd number"); //print
+       } else { //if isOdd is false
+           System.out.println(num + " is an even number"); //print
+       }
+
+
+    }
+    public static boolean isOdd(int num) { //method to check if number is odd
+        if(num % 2 == 0) { //if number is divisible by 2 && no reminder
+            return false; 
+        } else {
+            return true;
+        }
     }
     
 }
