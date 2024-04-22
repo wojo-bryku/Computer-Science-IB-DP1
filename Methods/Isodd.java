b99f78239beedad8612ffe61e8f6afa8e28a6d4d
scQC <alek.de.mehlem@gmail.com>
2024-04-22 11:17:38 +0200

    name

--- Methods/Isodd.java
+++ Methods/Isodd.java
@@ -1,8 +1,8 @@
 // Introduction to methods JAVA
 
-public class Intromet {
+public class Isodd {
     public static void main(String[] args) {
-        
+        System.out.println("Hello World");
     }
     
 }
