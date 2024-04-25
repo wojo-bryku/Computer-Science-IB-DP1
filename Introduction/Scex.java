1df7feb5e8dbc6df266e56e9300fc851830d2a18
scQC <alek.de.mehlem@gmail.com>
2024-04-25 14:56:49 +0200

    old fixes

--- Introduction/Scex.java
+++ Introduction/Scex.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
-public class ScannerEX{
+public class Scex{
   public static void main(String[] args){
     Scanner reader = new Scanner(System.in);
     System.out.println("Sup; whats your bloody name? ");
     String name = reader.nextLine();
 
-    System.out.println("Sup;" + name);
-
+    System.out.println("Sup; " + name);
+    System.out.println("How old are ya? ");
+    int age = reader.nextInt();
 
 
 
