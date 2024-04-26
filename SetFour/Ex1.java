3b2cd23f1bf3cca525d3354a6469886a0cd662dd
scQC <alek.de.mehlem@gmail.com>
2024-04-26 09:45:28 +0200

    ex2

--- SetFour/Ex1.java
+++ SetFour/Ex1.java
@@ -3,11 +3,23 @@ import java.util.Scanner;
 
 public class Ex1 {
     public static void main(String[] args) {
-        String word = "Hello";
+        
+        String word = "Hello World!";
         for(int i = 0; i < word.length(); i++)
         {
-            String singleChar = word.substring(i, i + 1);
-            System.out.println(singleChar);
+            String singleChar = word.substring(i, i + 1); 
+            System.out.print(singleChar);
+            try { //
+                Thread.sleep(205); // sleep for 25 milliseconds
+            } catch (InterruptedException e) { // catch the error  
+                e.printStackTrace(); // print the error message
+            }
         }
+
+
+        
+
+        
+
     }
 }
