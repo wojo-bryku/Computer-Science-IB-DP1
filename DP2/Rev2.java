ebbb6de8115cabbf9e30a87716c370ee313f5fd5
scQC <alek.de.mehlem@gmail.com>
2024-09-11 13:52:06 +0200

    dp2

---
+++ DP2/Rev2.java
@@ -0,0 +1,17 @@
+package DP2;
+import java.util.Scanner;
+
+
+public class Rev2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ur int: ");
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){//
+        System.out.println(i);
+        }
+
+    }
+    
+}
--- DP2/review/Review.java
+++
@@ -1,8 +0,0 @@
-package DP2.review;
-
-public class Review {
-    public static void main(String[] args) {
-        
-        
-    }    
-}
